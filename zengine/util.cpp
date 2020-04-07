2275c24980a86b26810169e3c877c0181b0d347d
zhonghua <zhonghua@bogon.pwrd.com>
2020-04-07 17:51:38 +0800

    new ar

---
+++ zengine/util.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "util.h"
+
+using namespace Zengine;
+
+SimpleRand::SimpleRand()
+  :_rndDouble(0, 1)
+{
+}
+
+SimpleRand::SimpleRand(int min, int max)
+  :_rndInt(min, max)
+{
+}
+
+double SimpleRand::getDoubleRnd()
+{
+  return _rndDouble(_randEngine);
+}
+
+int SimpleRand::getIntRnd()
+{
+  return _rndInt(_randEngine);
+}
+
+Rect Utils::rect(int x, int y, int w, int h)
+{
+  Rect rect;
+  rect.x = x;
+  rect.y = y;
+  rect.w = w;
+  rect.h = h;
+
+  return rect;
+}
---
+++ zengine/util.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace Zengine{
+class SimpleRand{
+public:
+  SimpleRand(int min, int max);
+  SimpleRand();
+  
+  int getIntRnd();
+  double getDoubleRnd();
+protected:
+  boost::random::mt19937 _randEngine;
+  boost::random::uniform_int_distribution<> _rndInt;
+  boost::random::uniform_real_distribution<> _rndDouble;
+};
+
+class Utils{
+public:
+  static Rect rect(int x, int y, int w, int h);
+};
+};
